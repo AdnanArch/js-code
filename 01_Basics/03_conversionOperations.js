d9311d939e19473bb599158937a98a1b9ff23bd9
AdnanArch <thedynamiccoder@gmail.com>
2024-01-05 23:21:35 +0500

    conversion

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,57 @@
+// 3. Conversion Operations
+
+let score = 100
+let scoreString = String(score)
+
+// console.log(typeof score) // number
+// console.log(typeof scoreString) // string
+
+let valueInNumber = Number(scoreString)
+// console.log(typeof valueInNumber) // number
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn) // number
+// console.log(booleanIsLoggedIn) // true
+
+let someNumber = true
+let someString = String(someNumber)
+
+console.log(someNumber) // true
+console.log(someString) // "true"
+console.log(typeof someNumber) // number
+console.log(typeof someString) // string
+
+/* 
+Notes:
+    1. There are 3 types of conversion operations in JavaScript
+        String() converts a value to string
+        Number() converts a value to number
+        Boolean() converts a value to boolean
+
+    Some conversion examples to Number():
+    "33" => 33
+    "33a" => NaN (Not a Number)
+    "33.5" => 33.5
+    "33.5a" => NaN (Not a Number)
+    true => 1
+    false => 0
+    null => 0
+    undefined => NaN (Not a Number)
+    " " => 0
+
+    Some conversion examples to Boolean():
+    0 => false
+    1 => true
+    "33" => true
+    "" => false
+
+    Some conversion examples to String():
+    33 => "33"
+    true => "true"
+    false => "false"
+    null => "null"
+    undefined => "undefined"
+    0 => "0"
+*/
