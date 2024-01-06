2239b8b26e77d0bc04acb702932d558d0d62dee1
AdnanArch <thedynamiccoder@gmail.com>
2024-01-06 22:18:28 +0500

    string

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,22 @@
+let name = "Adnan"
+let repoCount = 20
+
+// String interpolation
+console.log(`My name is ${name} and I have ${repoCount} repositories on GitHub`);
+
+const gameName = "Cricket"
+
+console.log(gameName.length);
+
+// string methods
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("ket"));
+console.log(gameName.lastIndexOf("ket"));
+console.log(gameName.endsWith("ket"));
+console.log(gameName.includes("ket"));
+console.log(gameName.substring(2, 5));
+console.log(gameName.slice(2, 5));
+console.log(gameName.split("c"));
+console.log(gameName.replace("Cricket", "Football"));
